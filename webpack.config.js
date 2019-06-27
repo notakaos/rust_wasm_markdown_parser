6df97fd23d895c8d08b5ce6d1324c7d31a9efe1f
Nobutaka Oshiro <notakaos@stdout.me>
2019-06-28 01:46:39 +0900

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,13 @@ const CopyPlugin = require('copy-webpack-plugin')
 module.exports = {
   entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, "gh-pages"),
+    path: path.resolve(__dirname, "docs"),
     filename: "index.js",
   },
   mode: "development",
   plugins: [
     new CopyPlugin([
-      {from: 'index.html', to: 'index.html'},
-      {from: 'markdown_parser.js', to: 'markdown_parser.js'},
-      {from: 'markdown_parser_bg.wasm', to: 'markdown_parser_bg.wasm'},
+      {from: 'index.html', to: 'index.html'}
     ])
   ]
 }
