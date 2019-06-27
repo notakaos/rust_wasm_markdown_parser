c78df551d15a5459d83e991b7509b0b7cec63dc4
Nobutaka Oshiro <notakaos@stdout.me>
2019-06-27 15:05:35 +0900

    Implement markdown parser

---
+++ README.md
@@ -0,0 +1,17 @@
+# Rust markdown parser example
+
+from: https://www.youtube.com/watch?v=Mj6hTGo0oGc
+
+## Development
+
+```bash
+git clone <REPOSITORY_URL>
+cd markdown_parser
+cargo build
+```
+
+### format
+
+```bash
+cargo fmt
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,12 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+// #![feature(proc_macro, wasm_custom_section, wasm_import_module)]
+
+mod parser;
+
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn parse(input: &str) -> String {
+    let result = parser::parse(input.to_string());
+    result
 }
